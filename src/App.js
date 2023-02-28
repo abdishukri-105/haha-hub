aa56223a26670a85053c9861a3164a628f172992
abdishukri <abdishukrimohamed105@gmail.com>
2023-02-28 17:04:15 +0300

    added tailwind and setup components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
-
+import Navbar from "./components/Navbar"
+import Meme from "./components/Meme"
+import Login from "./components/Login"
+import Register from "./components/Register"
 function App() {
   return (
     <div className="App">
-      
+        <Navbar />
+        <Meme />
+        <Login />
+        <Register />
    </div>
   );
 }
---
+++ src/components/Meme.js
@@ -0,0 +1,17 @@
+const Meme = () => {
+    return (  
+       <div>
+
+        <h1>
+            why so serious ...... joker 
+        </h1>
+
+        <h1 className="text-5xl text-red-800 bg-slate-600  font-bold underline">
+           Hello world!
+        </h1>
+
+       </div>
+    );
+}
+ 
+export default Meme;
