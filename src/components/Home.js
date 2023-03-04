78e80dcb454d50839218f3745138cfc21b46f566
abdishukri <abdishukrimohamed105@gmail.com>
2023-03-04 23:09:39 +0300

    home ui update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,29 @@
-import Login from "./Login"
-import joker from "../images/joker.jpg"
+import Login from "./Login";
+import joker from "../images/laugh.jpg";
 
+const Home = ({ setIsAuthenticated, isAuthenticated, handleLogin }) => {
+  return (
+    <div className="overflow-hidden">
+      {isAuthenticated ? (
+        <div
+          className="h-screen bg-no-repeat bg-cover bg-center flex  justify-center"
+          style={{ backgroundImage: `url(${joker})` }}
+        >
+          <div className="text-3xl text-center mt-14 text-teal-500 w-2/3 font-bold">
+            <h1 className="mb-10">Welcome to the Haha Hub, my fellow clown!</h1>  
+            <h1>Prepare yourself for some side-splitting, gut-busting, and knee-slapping laughs!</h1>
+          </div>
+        </div>
+      ) : (
+        <div
+          className="bg-cover bg-center flex flex-col items-center justify-center text-white min-h-screen"
+          style={{ backgroundImage: `url(${joker})` }}
+        >
+          <Login setIsAuthenticated={setIsAuthenticated} handleLogin={handleLogin} />
+        </div>
+      )}
+    </div>
+  );
+};
 
-const Home = ({setIsAuthenticated, isAuthenticated,handleLogin}) => {
-
-    return ( 
-      <div>
-       {  isAuthenticated ?   <img src={joker} alt="joker" /> :
-        <div className=" bg-cover bg-center flex flex-col justify-cente items-center text-white"  >
-          <Login  setIsAuthenticated={setIsAuthenticated} handleLogin={handleLogin}/>
-        </div> 
-        }
-      </div>
-     )
-}
-
- 
 export default Home;
-
-// style={{ backgroundImage: `url(${joker})` }}
